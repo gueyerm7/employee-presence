9dd553ed9516a61474876bd1d11550f03422c8a5
Mouhamed Gueye <mouhamed.gueye@example.com>
2026-07-13 19:13:52 +0100

    Generate APP_KEY in entrypoint if missing

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,9 @@
 set -e
 
 if [ -f artisan ]; then
+    if [ -z "$APP_KEY" ] || [ "$APP_KEY" = "base64:" ] || [ ${#APP_KEY} -lt 20 ]; then
+        php artisan key:generate --force
+    fi
     php artisan migrate --force
 fi
 
