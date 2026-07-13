3269c25c7eba600842f7b81a64e54dc22e5f47dc
Mouhamed Gueye <mouhamed.gueye@example.com>
2026-07-13 19:02:01 +0100

    Add /debug route to test web middleware

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,18 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/debug', function () {
+    return response('Laravel OK');
+});
+
 Route::get('/', function () {
     $path = public_path('spa.html');
-    if (!file_exists($path)) {
-        return response('SPA not found at: ' . $path, 500);
-    }
     return response(file_get_contents($path), 200, ['Content-Type' => 'text/html']);
 });
 
 Route::get('/{any?}', function ($any = null) {
     $path = public_path('spa.html');
-    if (!file_exists($path)) {
-        return response('SPA not found at: ' . $path, 500);
-    }
     return response(file_get_contents($path), 200, ['Content-Type' => 'text/html']);
 })->where('any', '^(?!api|sanctum).*$');
